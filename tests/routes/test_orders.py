6ff04de993594a2365c712491ac439b633968ec6
Joshua-Luke Bevan <63861228+jbevan4@users.noreply.github.com>
2023-07-08 21:48:38 +0100

    fix: broken nested test

--- tests/routes/test_orders.py
+++ tests/routes/test_orders.py
@@ -11,15 +11,10 @@ async def test_create_order_route_success() -> None:
     order_in = OrderIn(amount=Decimal(10), country_of_origin=Country.australia)
     create_order_mock.execute.return_value = {}
 
-    get_create_order_use_case_mock = MagicMock(return_value=create_order_mock)
-
     # Use asyncio.run to run the asynchronous function and await the result
     response = await create_order_route(
-        order_in=order_in, create_order=get_create_order_use_case_mock
+        order_in=order_in, create_order=create_order_mock
     )
-
-    print(f"Response: {response}")
-    print(f"Expected: {create_order_mock.execute.return_value}")
 
     assert response == create_order_mock.execute.return_value
     create_order_mock.execute.assert_called_once_with(**order_in.dict())
