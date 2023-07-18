bf8b3b70dc7edaef6c8d82e2f4eff0f70001ea10
Joshua-Luke Bevan <63861228+jbevan4@users.noreply.github.com>
2023-07-18 20:43:40 +0100

    test: refactor unit test for new dbclient mocks

--- fastapi_project/repositories/order/sqlite.py
+++ fastapi_project/repositories/order/sqlite.py
@@ -1,46 +1,15 @@
-from sqlite3 import Connection
+from sqlmodel import Session
 from fastapi_project.domain.order import Order
 from fastapi_project.repositories.order.base import OrderRepository
 
 
 class SQLiteOrderRepository(OrderRepository):
-    def __init__(
-        self: "SQLiteOrderRepository", db_path: str, db_client: Connection
-    ) -> None:
-        self.db_path = db_path
-        self.db_client = db_client
+    def __init__(self: "SQLiteOrderRepository", session: Session) -> None:
+        self.session = session
 
     def add(self: "SQLiteOrderRepository", order: Order) -> None:
-        with self.db_client:
-            cursor = self.db_client.cursor()
-            cursor.execute(
-                "INSERT INTO orders "
-                "("
-                "id,"
-                "amount,"
-                "country_of_origin,"
-                "created_at,"
-                "provider_id,"
-                "provider,"
-                "status,"
-                "tax_amount,"
-                "tax_percentage,"
-                "final_amount_charged"
-                ")"
-                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                (
-                    str(order.id),
-                    order.amount,
-                    order.country_of_origin.value,
-                    order.created_at,
-                    str(order.provider_id) if order.provider_id else None,
-                    order.provider.value if order.provider else None,
-                    order.status.value,
-                    order.tax_amount,
-                    order.tax_percentage,
-                    order.final_amount_charged,
-                ),
-            )
+        self.session.add(order)
+        self.session.commit()
 
     def update(self: "SQLiteOrderRepository", order: Order) -> Order:
         pass
--- tests/unit/respositories/order/test_sqlite.py
+++ tests/unit/respositories/order/test_sqlite.py
@@ -1,49 +1,23 @@
 from decimal import Decimal
-import pytest
 from unittest.mock import MagicMock
+import pytest
 from fastapi_project.domain.order import Country, Order, OrderIn
 from fastapi_project.domain.order_factory import OrderFactory
 from fastapi_project.repositories.order.sqlite import SQLiteOrderRepository
 
 
 @pytest.fixture
-def db_client() -> MagicMock:
-    # Create a mock SQLite client
+def db_client() -> None:
     return MagicMock()
 
 
 def test_add_order(db_client: MagicMock) -> None:
-    repository = SQLiteOrderRepository(db_client=db_client, db_path="")
+    repository = SQLiteOrderRepository(session=db_client)
+
     order_in = OrderIn(country_of_origin=Country.usa, amount=Decimal(10))
-    order: Order = OrderFactory.make_order(order_in=order_in)
+    order = OrderFactory.make_order(order_in=order_in)
+
     repository.add(order)
 
-    cursor = db_client.cursor.return_value
-    cursor.execute.assert_called_once_with(
-        "INSERT INTO orders "
-        "("
-        "id,"
-        "amount,"
-        "country_of_origin,"
-        "created_at,"
-        "provider_id,"
-        "provider,"
-        "status,"
-        "tax_amount,"
-        "tax_percentage,"
-        "final_amount_charged"
-        ")"
-        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        (
-            str(order.id),
-            order.amount,
-            order.country_of_origin.value,
-            order.created_at,
-            order.provider_id,
-            order.provider,
-            order.status.value,
-            order.tax_amount,
-            order.tax_percentage,
-            order.final_amount_charged,
-        ),
-    )
+    db_client.add.assert_called_once_with(order)
+    db_client.commit.assert_called_once()
