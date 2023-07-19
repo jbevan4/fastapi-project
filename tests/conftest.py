5cb98403ab67f7897570bd0b6054f2ba3af2f4b9
Joshua-Luke Bevan <63861228+jbevan4@users.noreply.github.com>
2023-07-19 20:06:29 +0100

    test: fix some broken files

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi_project.repositories.database import init_db, get_session
-
-
-@pytest.fixture(scope="session")
-def db_session() -> None:
-    init_db()
-    with get_session() as session:
-        yield session
