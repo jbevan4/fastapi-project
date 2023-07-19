dc6c46410a0a0174c49f97fcb6f65843f2ef5598
Joshua-Luke Bevan <joshua-luke.bevan@bark.com>
2023-07-19 20:28:22 +0100

    test: add in memory and sqlite flags to fixtures

--- tests/integration/routes/test_orders.py
+++ tests/integration/routes/test_orders.py
@@ -21,12 +21,19 @@ def test_client() -> TestClient:
 
 @pytest.fixture
 def sqlite_db() -> None:
+    Config.REPOSITORY_TYPE = RepositoryType.SQLITE
     init_db()
     yield
     cleanup_db()
 
 
-def test_create_order_with_in_memory_db(test_client: TestClient) -> None:
+@pytest.fixture
+def in_memory_db() -> None:
+    Config.REPOSITORY_TYPE = RepositoryType.IN_MEMORY
+    return
+
+
+def test_create_order_with_in_memory_db(test_client: TestClient, in_memory_db) -> None:
     # IN_MEMORY is the default database provided
     order_in: OrderIn = OrderIn(amount=Decimal(10), country_of_origin=Country.uk)
     response = test_client.post("/orders/", content=order_in.json())
@@ -40,15 +47,16 @@ def test_create_order_with_in_memory_db(test_client: TestClient) -> None:
     assert response_data.get("status") == Status.charged
 
 
-def test_create_order_with_sqlite_db(test_client: TestClient) -> None:
+def test_create_order_with_sqlite_db(test_client: TestClient, sqlite_db) -> None:
     order_in: OrderIn = OrderIn(amount=Decimal(10), country_of_origin=Country.uk)
 
     response = test_client.post("/orders/", content=order_in.json())
     response_data = response.json()
 
-    now = datetime.utcnow()
     created_at = datetime.strptime(response_data["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+    max_created_at = datetime.utcnow() + timedelta(seconds=1)
 
-    assert now - timedelta(seconds=5) <= created_at <= now + timedelta(seconds=10)
     assert response.status_code == HTTPStatus.OK
+    assert created_at <= max_created_at
+    assert response_data.get("amount") == order_in.amount
     assert response_data.get("status") == Status.charged
